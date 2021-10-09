98dc4bd4353e8e710f75f0186fffce8063b326ed
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2021-10-08 21:25:55 -0300

    vaalidações de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.http.response import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario
 
 
 def cadastro(request):
@@ -8,3 +9,30 @@ def cadastro(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    
+    #VERIFICAR SE O EMAIL DO USUARIO JA EXISTE
+    usuario = Usuario.objects.filter(email = email)
+
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=1')
+    
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=2')
+    
+    if len(senha) < 8 or len(senha) > 12:
+        return redirect('/auth/cadastro/?status=3')
+    
+    try:
+        senha = hashlib.sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome = nome,
+                          email = email,
+                          senha = senha)
+        usuario.save()
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
